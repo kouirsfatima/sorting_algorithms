9552f4ca4bf758df93f4476873cce67637a589a6
kouirsfatima <fatimaberline535@gmail.com>
2023-10-20 09:28:55 +0100

    bobbel sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,21 +12,20 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp,  sorted;
+	int temp;
 
 	if (size == 0 || array == NULL)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		sorted = 1;
+
 
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 
 			{
-				sorted = 0;
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
@@ -34,7 +33,5 @@ void bubble_sort(int *array, size_t size)
 
 			}
 		}
-		if (sorted == 0)
-			break;
 	}
 }
