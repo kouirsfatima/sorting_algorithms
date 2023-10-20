a6e6722fda014d05aec2d0e67c2433afa0575fc5
kouirsfatima <fatimaberline535@gmail.com>
2023-10-20 10:03:43 +0100

    bobbel sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,7 +21,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		
 
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1 ; j++)
 		{
 			if (array[j] > array[j + 1])
 
---
+++ 2-selection_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+#include <stdio.h>
+
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: The array
+ * @size: The size of the array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+    
+}
