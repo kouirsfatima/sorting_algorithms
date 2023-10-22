e9f9115d174439c6c7819e32de297a0179ca5214
kouirsfatima <fatimaberline535@gmail.com>
2023-10-22 12:15:15 +0100

    0-bubble_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -24,13 +24,15 @@ int Quicksort(int *array, int draw, int high, size_t size)
 		if (array[p] < pivot)
 		{
 			i++;
-			if (i != p)
+			if (i < p)
 			{
 				temp = array[i];
 				array[i] = array[p];
 				array[p] = temp;
+                if (i != p)
 				print_array(array, size);
 			}
+            
 		}
 	}
 	if (i + 1 != high)
