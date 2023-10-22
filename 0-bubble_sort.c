49ca2532d1f7ffa9619e408b824f33eb32ee17ea
kouirsfatima <fatimaberline535@gmail.com>
2023-10-22 13:44:41 +0100

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,11 +16,9 @@ void bubble_sort(int *array, size_t size)
 	if (size == 0 || array == NULL)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < (size - 1); i++)
 	{
-
-      
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 
